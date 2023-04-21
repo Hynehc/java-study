bc3c2f6a5ac3fa94ff6d9b24f966b61220f7fb0f
Hynehc <112534356+Hynehc@users.noreply.github.com>
2023-04-21 09:51:52 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("xxx is studying java!");
+	}
+}
+class ads{
+	public static void main(String[] args){
+		System.out.println("ads");
+	}
+}
+class God{
+	public static void main(String[] args){
+		System.out.println("God\n");
+	}
+}
